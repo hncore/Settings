8cc644c66db299555e78f85ac8b13d721fe93e04
Muhammad Asif <capripio@gmail.com>
2017-06-01 10:45:55 +0500

    Adding uploading image in model

--- src/app/Models/Setting.php
+++ src/app/Models/Setting.php
@@ -4,6 +4,8 @@ namespace Backpack\Settings\app\Models;
 
 use Backpack\CRUD\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class Setting extends Model
 {
@@ -11,4 +13,25 @@ class Setting extends Model
 
     protected $table = 'settings';
     protected $fillable = ['value'];
+
+
+    public function setValueAttribute($value)
+    {
+        $attribute_name = "value";
+        $disk = "public";
+        $destination_path = $this->key;
+        if ($value==null) {
+            Storage::disk($disk)->delete($this->image);
+            $this->attributes[$attribute_name] = null;
+        }
+        if (starts_with($value, 'data:image'))
+        {
+            $image = Image::make($value);
+            $filename = md5($value.time()).'.jpg';
+            Storage::disk($disk)->put($destination_path.'/'.$filename, $image->stream());
+            $this->attributes[$attribute_name] = $destination_path.'/'.$filename;
+        }else{
+            $this->attributes[$attribute_name] = $value;
+        }
+    }
 }
