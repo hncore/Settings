1c699146d03cd45c3ecc50317e43f4e38575a8b8
Soufiene Slimi <soufiene.slimi@mail.com>
2020-06-07 01:05:55 +0100

    Add support for relationship field
    Add support for relationship by using select2_from_array field

--- src/app/Http/Controllers/SettingCrudController.php
+++ src/app/Http/Controllers/SettingCrudController.php
@@ -50,6 +50,23 @@ class SettingCrudController extends CrudController
             ],
         ]);
 
-        CRUD::addField(json_decode(CRUD::getCurrentEntry()->field, true));
+        $data = json_decode(CRUD::getCurrentEntry()->field, true);
+        // add support for relationship by using select2_from_array field
+        // if this field is nor really an normal select2_from_array
+        if ('select2_from_array' == $data['type'] && ! is_array($data['options'])) {
+            $options = explode('|', $data['options']);
+            // EX: App\\Models\\Tag or App\\Models\\Tag|name or  App\\Models\\Tag|name|id
+            $model = $options[0];
+            $attribute = $options[1] ?? 'name';
+            $id = $options[2] ?? 'id';
+            $data['options'] = (new $model())::all([$id, $attribute])->pluck($attribute, $id)->toArray();
+            // also support multiple entries by casting the value field
+            // add the cast
+            $this->crud->getModel()->withCasts(['value' => 'array']);
+            // remove the current entry to reloaded it and apply the cast
+            $this->crud->entry = null;
+        }
+
+        CRUD::addField($data);
     }
 }
